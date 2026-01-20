5d5ec16f91c6b45537d95f584e93d3cd77c7cb78
Moeketsi Leanard Mmako <moeketsimmako1@gmail.com>
2026-01-20 10:12:23 +0200

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,9 @@
+word = "The!quick!brown!fox!jumps!over!the!lazy!dog!"
+rep_word = word.replace("!", " ")
+print(rep_word)
+
+upper_word = rep_word.upper()
+print(upper_word)
+
+reversed_word = upper_word[::-1]
+print(reversed_word)
